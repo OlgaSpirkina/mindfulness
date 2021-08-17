edc6ec7b23abeb669b7728f06ccdaa274e074013
olga <>
2021-08-17 20:05:11 +0200

    add toggle button to Quotes.js: initially 8 cards are displayed, toggle all cards onClick or first 8

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import Quote from './Quote'
 import styles from './Quote.module.css'
 
 
 const Quotes = ({ filteredQuotes }) => {
+  const [toggle, setToggle] = useState(true);
   return(
-    <section className={styles.quote_section}>
-    {filteredQuotes.map((quote, index) => {
-      return(
-        <div key={index} className={styles.item_quote_div}>
-          <Quote
-            quote={quote}
-          />
-        </div>
-      )
-  })}
-    </section>
+    <Fragment>
+      <section className={styles.quote_section}>
+      {filteredQuotes.map((quote, index) => {
+        if(index <= 8){
+          return(
+            <div key={index} className={styles.item_quote_div}>
+              <Quote
+                quote={quote}
+              />
+            </div>
+          )
+        }return(
+          <div
+            key={index}
+            className={toggle ? `${styles.item_quote_div} ${styles.invisible}` : `${styles.item_quote_div}`}
+          >
+            <Quote quote={quote} />
+          </div>
+        )
+    })}
+      </section>
+      <div className={styles.seemore}>
+        <button onClick={()=>setToggle(!toggle)} >See more</button>
+      </div>
+    </Fragment>
   )
 }
 Quotes.propTypes = {
