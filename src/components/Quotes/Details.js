42f15cc11583798f12c5952a046256dca4b283ed
olga <>
2021-08-25 12:41:50 +0200

    delete audio files from db.json and import them to Details component, create an array of sounds

--- src/components/Quotes/Details.js
+++ src/components/Quotes/Details.js
@@ -1,34 +1,56 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import { useParams } from 'react-router'
+import breathingSpace from "../../audio/breathing-space.mp3"
+import breathingSpace3min from "../../audio/breathing-space-3min.mp3"
+import breathing3min from "../../audio/breathing3min.mp3"
+import breathing5min from "../../audio/breathing5min.mp3"
+import breathing10min from "../../audio/breathing10min.mp3"
+import fiveMinBreathing from "../../audio/five-min-breathing.mp3"
+import fourMinBreathing from "../../audio/four-min-breathing.mp3"
+import sixMinAwarness from "../../audio/six-min-awarness.mp3"
+import sounds3min from "../../audio/sounds3min.mp3"
+import tenMinBreathing from "../../audio/ten-min-breathing.mp3"
+import tensionRelease5min from "../../audio/tension-release-5min.mp3"
 
 const Details = ({ quotes }) => {
   const { login } = useParams();
-  let detail = '';
+  let imageInDetails = '';
+  let audioFigcaption = '';
   let audio = '';
+  const soundArray = [
+    breathingSpace,
+    breathingSpace3min,
+    breathing3min,
+    breathing5min,
+    breathing10min,
+    fiveMinBreathing,
+    fourMinBreathing,
+    sixMinAwarness,
+    sounds3min,
+    tenMinBreathing,
+    tensionRelease5min
+  ];
   for(let i=0; i<quotes.length; i++){
     if(login === quotes[i].login){
-      detail = `${quotes[i].image}`;
-      audio = `${quotes[i].audio}`
-      console.log(`Voici: ${quotes[i].image}`);
+      imageInDetails = `${quotes[i].image}`;
+      audioFigcaption = `${quotes[i].audioFigcaption}`
+      for(let j=0; j<soundArray.length; j++){
+        if(i === j){
+          audio = soundArray[j];
+        }
+      }
     }
   }
-  useEffect(() => {
-    const audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.play()
-  })
     return(
       <Fragment>
         <h1 style={{marginTop: '5rem'}}>{login}</h1>
-        <img src={detail} alt="paysage" />
+        <img src={imageInDetails} alt="paysage" />
         <figure>
-          <figcaption>Listen to the T-Rex:</figcaption>
-            <audio controls
-              className="audio-element"
-            >
+          <figcaption>{audioFigcaption}</figcaption>
+            <audio controls>
               <source src={audio} type="audio/mpeg" />
             Your browser does not support the audio element.
             </audio>
-
         </figure>
 
       </Fragment>
